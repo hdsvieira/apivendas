6678148ec409622eedfab90151f34946133a8fb0
Henrique (Manauara) <henrique.vieira@semantix.com.br>
2021-10-14 19:14:37 -0400

    Finalizado 18. Desafio - Expondo API's Rest para buscar produtos no banco de dados

---
+++ src/main/java/com/gvendas/gestaovendas/servico/ProdutoServico.java
@@ -0,0 +1,25 @@
+package com.gvendas.gestaovendas.servico;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gvendas.gestaovendas.entidades.Produto;
+import com.gvendas.gestaovendas.repositorio.ProdutoRepositorio;
+
+@Service
+public class ProdutoServico {
+
+	@Autowired
+	private ProdutoRepositorio produtoRepositorio;
+	
+	public List<Produto> listarTodos() {
+		return produtoRepositorio.findAll();
+	}
+
+	public Optional<Produto> buscarPorCodigo(Long codigo) {
+		return produtoRepositorio.findById(codigo);
+	}
+}
